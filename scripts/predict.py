76a75dcdbd031a1d35c7f87c2214e08e97e908d2
Gabriel Oliveira <gabriel@umobi.com.br>
2025-10-14 13:35:33 -0300

    Adiciona suporte para execução do script sem instalação do pacote

--- scripts/predict.py
+++ scripts/predict.py
@@ -1,4 +1,12 @@
+import sys
+from pathlib import Path
 import argparse
+# Allow running the script without installing the package.
+project_root = Path(__file__).resolve().parents[1]
+src_path = project_root / "src"
+if str(src_path) not in sys.path:
+    sys.path.append(str(src_path))
+
 from mlops_pipeline.inference.predict import predict_file
 
 if __name__ == "__main__":
